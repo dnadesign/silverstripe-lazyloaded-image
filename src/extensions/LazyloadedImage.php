df4895ee71d0755883d473f66a93f42c415cefcc
Georgy Malanichev <georgy@malanichev.com>
2019-02-25 17:15:05 +1300

    Fix incorrect extension being loaded

--- src/extensions/LazyloadedImage.php
+++ src/extensions/LazyloadedImage.php
@@ -7,14 +7,14 @@ use SilverStripe\View\Requirements;
 
 class LazyloadedImageExtension extends Extension
 {
-    public function getLazyloaded()
-    {
-        Requirements::javascript('dnadesign/silverstripe-lazyloaded-image:client/javascript/lazysizes.min.js');
-        Requirements::javascript('dnadesign/silverstripe-lazyloaded-image:client/javascript/ls.blur-up.min.js');
-        Requirements::css('dnadesign/silverstripe-lazyloaded-image:client/css/lazysizes-blur.css');
+  public function getLazyloaded()
+  {
+    Requirements::javascript('dnadesign/silverstripe-lazyloaded-image:client/javascript/lazysizes.min.js');
+    Requirements::javascript('dnadesign/silverstripe-lazyloaded-image:client/javascript/ls.blur-up.min.js');
+    Requirements::css('dnadesign/silverstripe-lazyloaded-image:client/css/lazysizes-blur.css');
 
-        return $this->owner->customise([
-            'LQIP' => $this->owner->Quality(20)
-        ])->renderWith('Includes/LazyloadedImage');
-    }
+    return $this->owner->customise([
+      'LQIP' => $this->owner->Quality(20)
+    ])->renderWith('Includes/LazyloadedImage');
+  }
 }
