a8ae4360723f139ecd0b2989e969e4e4373a87fd
Georgy Malanichev <georgy@malanichev.com>
2019-04-18 17:09:55 +1200

    Remove lazyload functionality from SizedImage

--- src/models/SizedImage.php
+++ src/models/SizedImage.php
@@ -7,64 +7,51 @@ use SilverStripe\Assets\Image;
 
 class SizedImage extends DataObject
 {
-	private static $table_name = 'SizedImage';
+  private static $table_name = 'SizedImage';
 
-	private static $db = [
-		'Name' => 'Varchar(255)',
-		'Size' => 'Varchar(100)',
-		'Rule' => 'Varchar(100)'
-	];
+  private static $db = [
+    'Name' => 'Varchar(255)',
+    'Size' => 'Varchar(100)',
+    'Rule' => 'Varchar(100)'
+  ];
 
-	private static $has_one = [
-		'Image' => Image::class
-	];
+  private static $has_one = [
+    'Image' => Image::class
+  ];
 
-	private static $owns = [
-        'Image'
-    ];
+  private static $owns = [
+    'Image'
+  ];
 
-	private static $summary_fields = [
-		'ID' => 'ID',
-		'Size' => 'Size',
-		'Image.CMSThumbnail' => 'Thumbnail'
-	];
+  private static $summary_fields = [
+    'ID' => 'ID',
+    'Size' => 'Size',
+    'Image.CMSThumbnail' => 'Thumbnail'
+  ];
 
-	public function getCMSFields()
-	{
-		$fields = parent::getCMSFields();
+  public function getCMSFields()
+  {
+    $fields = parent::getCMSFields();
 
-		$name = $fields->dataFieldByName('Name');
-		$name->setDescription('For reference only');
+    $name = $fields->dataFieldByName('Name');
+    $name->setDescription('For reference only');
 
-		return $fields;
-	}
+    return $fields;
+  }
 
-	/**
-	* This object should exists only if it has an image
-	*/
-	public function exists()
-	{
-		return $this->Image() && $this->Image()->exists();
-	}
+  /**
+  * This object should exists only if it has an image
+  */
+  public function exists()
+  {
+    return $this->Image() && $this->Image()->exists();
+  }
 
-	/**
-	* Proxy method to be able to render the lazyloaded template
-	* @see DNADesign\Images\Extensions\LazyloadedImage
-	*/
-	public function getLazyloaded()
-	{
-		if ($this->Image()->exists() && $this->Image()->hasMethod('getLazyloaded')) {
-			return $this->Image()->getLazyloaded();
-		}
-
-		return $this->Image();
-	}
-
-	/**
-	* Render normal image by default
-	*/
-	public function forTemplate()
-	{
-		return ($this->Image()->exists()) ? $this->Image()->fortemplate() : '';
-	}
+  /**
+  * Render normal image by default
+  */
+  public function forTemplate()
+  {
+    return ($this->Image()->exists()) ? $this->Image()->fortemplate() : '';
+  }
 }
--- src/templates/DNADesign/Images/Models/MultipleSizeImage.ss
+++ src/templates/DNADesign/Images/Models/MultipleSizeImage.ss
@@ -1,15 +1,6 @@
-<% if $Lazyload %>
 <picture>
-	<% loop $Sources %>
-		<source media="($Rule)" data-srcset="$Image.URL">
-	<% end_loop %>
-	$BaseImage.Lazyloaded
-</picture>
-<% else %>
-	<picture>
 	<% loop $Sources %>
 		<source media="($Rule)" srcset="$Image.URL">
 	<% end_loop %>
-	$BaseImage
+	{$BaseImage}
 </picture>
-<% end_if %>
